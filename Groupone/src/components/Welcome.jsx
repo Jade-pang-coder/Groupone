c35d570f13121f36b5e8f0d327695f5e671183db
Jade <pangqihui52@hotmail.com>
2026-06-27 11:09:36 +0800

    welcome

--- Groupone/src/components/Welcome.jsx
+++ Groupone/src/components/Welcome.jsx
@@ -1,1 +1,14 @@
 // to implement
+
+import React from 'react';
+
+function Welcome() {
+  return (
+    <div>
+      <h2>Welcome to Group One'swebsite!</h2>
+      <p>We are learning how to build websites using React. This is our first group project, and we are excited to add more features to it as we keep practicing our coding skills.</p>
+    </div>
+  );
+}
+
+export default Welcome;
